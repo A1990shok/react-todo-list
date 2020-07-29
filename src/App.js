641018d6f424a769ffa23c9232928d27f860dae9
ashok kumar <ashokyadavaky11@gmail.com>
2020-07-29 14:09:19 +0530

    showing vs-code github setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
+// showing vs-code github setup
+
 function App() {
   return (
     <div>
